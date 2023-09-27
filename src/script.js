5c39c3bdff6d4f4c7e0755583f98906461661ee9
Killian <Killianouzet@gmail.com>
2023-09-27 22:53:34 +0200

    changement du chemin pour la texture du diams

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ const camera = new THREE.PerspectiveCamera(70, iw / ih)
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 
-const texture = new THREE.TextureLoader().load('diamond.jpg')
+const texture = new THREE.TextureLoader().load('../public/diamond.jpg')
 const material = new THREE.MeshPhongMaterial({ map:texture })
 
 const mesh = new THREE.Mesh(geometry, material)
