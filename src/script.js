85f14a00a2d60ba3f10cc15b9ca980e409239ef7
Killian <Killianouzet@gmail.com>
2023-09-28 19:53:47 +0200

    vagues de points avec du cos et sin :C

--- src/script.js
+++ src/script.js
@@ -5,29 +5,77 @@ const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(70, iw / ih)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+const geometry = computeGeometry()
+const material = new THREE.PointsMaterial({size:0.015, vertexColors: true})
+const mesh = new THREE.Points(geometry, material)
 
-const texture = new THREE.TextureLoader().load('src/public/diamond.jpg')
-const material = new THREE.MeshPhongMaterial({ map:texture })
-
-const mesh = new THREE.Mesh(geometry, material)
-
-const light = new THREE.PointLight(0xeeeeee, 2)
-
-scene.add(light)
 scene.add(mesh)
 
-camera.position.set(0, 0, 3)
-light.position.set(0, 0, 3)
+camera.position.set(0, 1, 2)
+camera.lookAt(0, -0.5, 0)
 
 const renderer = new THREE.WebGLRenderer({ canvas })
 
+const clock = new THREE.Clock()
+let t = 0
 
 loop()
 
 function loop() {
-  requestAnimationFrame(loop)
-  mesh.rotation.x += 10
-  mesh.rotation.y += 10
+  t += clock.getDelta()
+  animeGeometry(geometry, t)
+  mesh.rotation.y = 0.1*t
   renderer.render(scene, camera)
+  requestAnimationFrame(loop)
+}
+
+function computeGeometry() {
+  const space = 4, nb = 100, amp = 0.1, fre = 1, pi2 = Math.PI*2
+
+  const geometry = new THREE.BufferGeometry()
+
+  const positions = new Float32Array(nb*nb*3)
+  const colors = new Float32Array(nb*nb*3)
+
+  let k=0
+  for(let i=0; i<nb; i++){
+    for(let j=0; j<nb; j++){
+      const x = i*(space/nb)-space/2
+      const z = j*(space/nb)-space/2
+      const y = amp*(Math.cos(x*pi2*fre)+Math.sin(z*pi2*fre))
+      positions[ 3*k+0 ] = x
+      positions[ 3*k+1 ] = y
+      positions[ 3*k+2 ] = z
+      const intensity = (y/amp)/2+0.3
+      colors[ 3*k+0 ] = j/nb * intensity
+      colors[ 3*k+1 ] = 0
+      colors[ 3*k+2 ] = i/nb * intensity
+      k++
+    }
+  }
+  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+  return geometry
+}
+
+function animeGeometry(geometry, progress) {
+  const space = 4, nb = 100, amp = 0.1, pi2 = Math.PI*2
+  const phase = progress
+  const fre = 0.8 + Math.cos(progress)/2
+
+  let k=0
+  for(let i=0; i<nb; i++){
+    for(let j=0; j<nb; j++){
+      const x = i*(space/nb)-space/2
+      const z = j*(space/nb)-space/2
+      const y = amp*(Math.cos(x*pi2*fre+phase)+Math.sin(z*pi2*fre+phase))
+      geometry.attributes.position.setY(k, y)
+      const intensity = (y/amp)/2+0.3
+      geometry.attributes.color.setX(k, j/nb * intensity)
+      geometry.attributes.color.setZ(k, i/nb * intensity)
+      k++
+    }
+  }
+  geometry.attributes.position.needsUpdate = true
+  geometry.attributes.color.needsUpdate = true
 }
